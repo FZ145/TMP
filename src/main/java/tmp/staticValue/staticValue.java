e74bc9cb2eea86c6a70d7a6b5ee2c05bf72ad6e7
cuishining <kingszelda@163.com>
2015-11-21 15:59:45 +0800

    fix bugs

--- src/main/java/tmp/staticValue/staticValue.java
+++ src/main/java/tmp/staticValue/staticValue.java
@@ -12,6 +12,7 @@ public class StaticValue {
     public static final int ACTIVE_TIMES_THRESHOLD = 100;
     // 推荐实体与请求实体没有信任的时候，默认为0.5
     public static final BigDecimal DEFAULT_TRUST_VALUE = new BigDecimal(0.5);
+    //若云的声誉取值为【0,1】则云只有衰减作用了，这样会导致一直衰减下去，因此这里默认为1，取值范围为【0.5，1.5】
     public static final BigDecimal DEFAULT_PROVIDER_TRUST_VALUE = new BigDecimal(1);
     private StaticValue() {
     }
