9a18bdb956cf1afd97fa4dadc947517f460d32f6
yuanyao <yuanyaosmile@gmail.ocm>
2016-03-19 23:38:45 +0800

    注册功能的实现（仅用户与云可以注册，前端仍是jsp）。

--- src/main/java/tmp/controller/RegiterController.java
+++ src/main/java/tmp/controller/RegiterController.java
@@ -1,13 +1,19 @@
 package tmp.controller;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import tmp.dao.ComponentMapper;
 import tmp.dao.ProviderMapper;
 import tmp.dao.RenterMapper;
+import tmp.entity.Component;
+import tmp.entity.Provider;
 import tmp.entity.Renter;
 import tmp.service.RegisterService;
 import org.springframework.web.bind.annotation.RequestMethod;
+import tmp.staticvalue.StaticValue;
+
 import javax.annotation.Resource;
 import javax.resource.spi.Connector;
 
@@ -17,19 +23,32 @@ import javax.resource.spi.Connector;
 @Controller
 @RequestMapping(value = "/user")
 public class RegiterController {
+
     @Resource
-  private RegisterService registerService;
-    @Resource
-    private RenterMapper renterMapper;
-    private  String uid;
-    private  String password;
+    private ProviderMapper providerMapper;
+
+   @Resource
+   private  RenterMapper renterMapper;
+
 
     @RequestMapping( value = "/RegisterController.do", method = RequestMethod.POST)
-    public void register(String uid, String password, String indentifyCode) throws Exception {
-                Renter renter = new Renter();
-                renter.setUid(uid);
-                renter.setPassword(password);
-                renterMapper.insert(renter);
-            //registerService.register();
+    public ModelAndView register(String uid, String password, String indentifyCode) throws Exception {
+        ModelAndView modelAndView = new ModelAndView();
+        if (StringUtils.equals(indentifyCode, StaticValue.RENTER)) {
+            Renter renter = new Renter();
+            renter.setUid(uid);
+            renter.setPassword(password);
+            renterMapper.insert(renter);
+
+
+        }else if(StringUtils.equals(indentifyCode, StaticValue.PROVIDER)){
+            Provider provider = new Provider();
+            provider.setUid(uid);
+            provider.setPassword(password);
+            providerMapper.register(provider);
+
+        }
+        modelAndView.setViewName("registerSuccess");
+        return modelAndView;
     }
 }
